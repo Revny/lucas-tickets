0d3167a4761a8e2b05d3565143f2d15c579c9d67
Revny <Lsilver205@gmail.com>
2025-09-16 11:25:27 -0300

    Add project files.

---
+++ lucas tickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lucas_tickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
