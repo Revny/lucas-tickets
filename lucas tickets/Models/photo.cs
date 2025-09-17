7f7fb5293794bbfb3b215e3793849796c59e2070
Revny <Lsilver205@gmail.com>
2025-09-17 10:13:14 -0300

    k

---
+++ lucas tickets/Models/photo.cs
@@ -0,0 +1,11 @@
+﻿namespace lucas_tickets.Models
+{
+    public class Photo
+    {
+        public int PhotoId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } =string.Empty;
+        public string Filename { get; set; } = string.Empty;
+        public DateTime CreateDate { get; set; }
+    }
+}
